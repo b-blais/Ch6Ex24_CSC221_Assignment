b901ece97f576a8fe1e23fb720a3c55499f07f92
Brion <40612857+Drazzot@users.noreply.github.com>
2024-11-09 13:08:23 -0500

    Rewrite of a portion of logic and added more robust menus array and file functions.

--- Ch6Ex24_CSC221_Assignment.cpp
+++ Ch6Ex24_CSC221_Assignment.cpp
@@ -1,5 +1,5 @@
 /*
-Title: Chapter 6 Exercise 24 - Rock. Paper , Scissors Part 2
+Title: Chapter 7 - Rock. Paper , Scissors Part 3
 File Name : Ch6Ex24_CSC221_Assignment
 Programmer : Brion Blais
 Date : 11 / 2024
@@ -23,40 +23,82 @@ accept the name of the player and keep count of how many times they
 have won. Modify your program using a new branch of your project so that
 you preserve the orginal main branch from Part 1.
 Submit the code file as well as a screenshot of your testing
+
+Part 3:
+Using your program from the Rock Paper Scissors Part 2 create
+
+a new branch that uses a file to store the results of each new session along
+with previous sessions.
+When starting you should provide a menu that
+that lets the user start a new session, display previous sessions for yourself of any others.
  */
 
 #include <iostream>
 #include <random>
+#include <fstream>
+#include <string>
 using namespace std;
 
+int gameMenu();
 int randomNumber();
 string computerChoiceFunc();
 string userChoiceFunc();
 string theGameEngine(string, string);
 string getPlayerName();
 void totalWins(string);
+void openRead();
+void openWrite();
 
-int playerWins = 0;
-int computerWins = 0;
-int ties = 0;
+int gameSummary[] = {0,0,0};
+fstream fileStreamObject;
+
 
 int main()
 {
-    
-    string playerName = getPlayerName();
-    cout << "Welcome to the Rock, Paper, Scissors game " << playerName << "!\n\n";
-    string userChoice;
-    while (userChoice != "Game Over") {
-        string computerChoice = computerChoiceFunc();
-        userChoice = userChoiceFunc();
-        if (userChoice == "Game Over") {
-            continue;
+    int menuChoice;
+    //openRead();
+    do {
+        menuChoice = gameMenu();
+        if (menuChoice == 1) {
+            string playerName = getPlayerName();
+            cout << "Starting a new Rock, Paper, Scissors game " << playerName << "!\n\n";
+            string userChoice;
+            while (userChoice != "Main Menu") {
+                string computerChoice = computerChoiceFunc();
+                userChoice = userChoiceFunc();
+                if (userChoice == "Main Menu") {
+                    continue;
+                }
+                string winner = theGameEngine(computerChoice, userChoice);
+                totalWins(winner);
+            }
+            openWrite();
+            fileStreamObject << playerName << "\t   " << gameSummary[0] << "\t    " << gameSummary[1] << "\t     " << gameSummary[2] << "\n";
+            fileStreamObject.close();
         }
-        string winner = theGameEngine(computerChoice, userChoice);
-        totalWins(winner);
-    }
+        else if (menuChoice == 2) {
+            string fileContents;
+            cout << "********************************\n";
+            cout << "         Previous Games\n";
+            cout << "********************************\n";
+            cout << "Player    Wins    Losses   Ties\n";
+            openRead();
+            while (getline(fileStreamObject, fileContents)) {
+                cout << fileContents << endl;
+            }
+            cout << "********************************\n\n\n";
+            fileStreamObject.close();
+        }
+        else if (menuChoice == 3) {
+            cout << "Exiting ...\n";
+            break;
+        }
+        else cout << "Program error";
+        cout << "\n";
         
-    cout << "Game Over\n";
+    
+    } while (menuChoice > 0 && menuChoice < 4);
+    
     return 0;
 }
 
@@ -81,21 +123,18 @@ string computerChoiceFunc() {
 string userChoiceFunc() {
     int userSelection;
     do {
-        cout << "Choose your weapon!\n";
-        cout << "-------------------\n";
+        cout << "  Choose your weapon!\n";
+        cout << "-----------------------\n";
         cout << "1\tRock\n";
         cout << "2\tPaper\n";
         cout << "3\tScissors\n";
-        cout << "-------------------\n";
-        cout << "If you are done playing, enter -1\n";
+        cout << "4\tReturn to Main Menu\n";
+        cout << "-----------------------\n";
         cin >> userSelection;
         cout << "\n";
     } 
-    while ((userSelection < 1 || userSelection > 3) && userSelection != -1);
+    while (userSelection < 1 || userSelection > 4);
     switch (userSelection) {
-    case -1:
-        return "Game Over";
-        break;
     case 1:
         return "Rock";
         break; //shouldn't be needed, as return should exit the switch
@@ -104,11 +143,32 @@ string userChoiceFunc() {
         break; 
     case 3:
         return "Scissors";
-        break; 
+        break;
+    case 4:
+        return "Main Menu";
+        break;
     default:
         cout << "Program error.";
         return "Error";
     }
+}
+
+int gameMenu() {
+    int userMenuSelection;
+    cout << "**********************\n";
+    cout << "*        ROCK        *\n";
+    cout << "*       PAPER        *\n";
+    cout << "*      SCISSORS      *\n";
+    cout << "**********************\n";
+    cout << "\n";
+    do {
+        cout << "Press 1 for a new game.\n";
+        cout << "Press 2 to view previous game results.\n";
+        cout << "Press 3 to exit.\n";
+        cin >> userMenuSelection;
+        cout << "\n";
+    } while (userMenuSelection < 1 || userMenuSelection > 3);
+    return userMenuSelection;
 }
 
 int randomNumber() {
@@ -120,8 +180,8 @@ int randomNumber() {
 string theGameEngine(string computerChoice, string userChoice) {
     string result;
     
-    cout << "The battle starts ... the computer has throws ** " << computerChoice << " **\n";
-    cout << "and you have thrown ** " << userChoice << " **\n";
+    cout << "The battle starts ... the computer has thrown ** " << computerChoice << " **\n";
+    cout << "                      ... and you have thrown ** " << userChoice << " **\n";
     
     if (computerChoice == "Rock" && userChoice == "Rock") {
         cout << "You both threw ** Rock **!! No winner, it's a tie.\t";
@@ -174,13 +234,32 @@ string getPlayerName() {
 
 void totalWins(string winner) {
     if (winner == "User Win") {
-        ++playerWins;
+        gameSummary[0]++; //player wins
     } 
     else if (winner == "Computer Win") {
-        ++computerWins;
+        gameSummary[1]++;//computerWins;
     }
     else if (winner == "Tie") {
-        ++ties;
+        gameSummary[2]++;//ties;
     }
-    cout << "\nYou have " << playerWins << " wins, " << computerWins << " losses, and " << ties << " ties.\n\n";
+    cout << "\n\nYou have " << gameSummary[0] << " wins, " << gameSummary[1] << " losses, and " << gameSummary[2] << " ties.\n\n";
+}
+
+void openWrite() {
+    string fileName = "previousGames.txt";
+    fileStreamObject.open(fileName, ios_base::app);
+    if (!fileStreamObject) {
+        cout << "The previousGames.txt file could not be found.\n";
+        cout << "Please check to see if the file is in the correct folder and rerun the program. Exiting ...\n";
+        exit(0);
+    }
+}
+void openRead() {
+    string fileName = "previousGames.txt";
+    fileStreamObject.open(fileName);
+    if (!fileStreamObject) {
+        cout << "The previousGames.txt file could not be found.\n";
+        cout << "Please check to see if the file is in the correct folder and rerun the program. Exiting ...\n";
+        exit(0);
+    }
 }
